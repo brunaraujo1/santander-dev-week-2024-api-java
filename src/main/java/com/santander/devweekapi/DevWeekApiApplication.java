95c2593466059f7b47a210ddac617f5a7f8b49f9
Bruna Araújo <bruna_13@live.com.pt>
2024-06-06 18:10:49 -0300

    Adicionando arquivos iniciais do projeto

---
+++ src/main/java/com/santander/devweekapi/DevWeekApiApplication.java
@@ -0,0 +1,13 @@
+package com.santander.devweekapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevWeekApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevWeekApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santander/devweekapi/DevWeekApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander.devweekapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevWeekApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
